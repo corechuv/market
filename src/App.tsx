e7739459368366aa4025ffe37a8cf67339e6aa78
corechuv <chuv.ole@icloud.com>
2025-08-11 13:45:53 +0200

    modified:   src/App.tsx 	modified:   src/components/Product/ProductItemList.module.scss 	modified:   src/pages/Product/Product.module.scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Product from './pages/Product/Product'
 import Home from './pages/Home/Home'
-import ProductList from './pages/Product/ProductsPage'
-import Search from './pages/Search/SearchPage'
+import ProductsPage from './pages/Product/ProductsPage'
+import SearchPage from './pages/Search/SearchPage'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import "./styles/scrollbar.module.scss" // Import global variables
@@ -17,10 +17,8 @@ export default function App() {
       <main className="app-container">
         <Routes>
           <Route path="/" element={<Home />} />
-
-          <Route path="/search" element={<Search />} />
-
-          <Route path="/products" element={<ProductList />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/products" element={<ProductsPage />} />
           <Route path="/product/1" element={<Product />} />
         </Routes>
       </main>
