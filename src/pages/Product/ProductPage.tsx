672fe5c4475fd8cbc8f5e3fef84918a41eefce55
corechuv <chuv.ole@icloud.com>
2025-08-26 16:56:20 +0200

    modified:   src/components/UI/Button.module.scss 	modified:   src/pages/Product/ProductPage.module.scss 	modified:   src/pages/Product/ProductPage.tsx

--- src/pages/Product/ProductPage.tsx
+++ src/pages/Product/ProductPage.tsx
@@ -271,7 +271,6 @@ export default function ProductPage() {
                     </div>
 
                     <Button
-                        className={cls.stickyCta__button}
                         disabled={!available}
                         onClick={handleAddToCart}
                     >
