f80e03e2635d8ab3138ffe9b19629942015ec7f6
corechuv <chuv.ole@icloud.com>
2025-09-25 15:32:26 +0200

    modified:   src/components/Icons/IconFilters.tsx

--- src/components/Icons/IconFilters.tsx
+++ src/components/Icons/IconFilters.tsx
@@ -4,10 +4,10 @@ export default function IconFilters(props: React.SVGProps<SVGSVGElement>): React
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {...props}>
       <g fill="currentColor">
-        <rect x="3" y="5.1" width="18" height="1.8" rx="0.9" />
-        <rect x="3" y="15.1" width="18" height="1.8" rx="0.9" />
-        <circle cx="8" cy="6" r="2.6" />
-        <circle cx="11" cy="16" r="2.6" />
+        <rect x="3" y="6.1" width="18" height="1.8" rx="0.9" />
+        <rect x="3" y="16.1" width="18" height="1.8" rx="0.9" />
+        <circle cx="8" cy="7" r="2.6" />
+        <circle cx="11" cy="17" r="2.6" />
       </g>
     </svg>
   );
