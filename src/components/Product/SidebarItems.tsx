06bddb3d52b9b29c9cd26fca1fc17b039f3105ba
corechuv <chuv.ole@icloud.com>
2025-10-26 02:20:22 +0100

    p

--- src/components/Product/SidebarItems.tsx
+++ src/components/Product/SidebarItems.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import Accordion from "../../components/Product/Accordion";
 import CheckboxGroup from "../../components/Product/CheckboxGroup";
 import PriceRangeDual from "../../components/Product/PriceRangeDual";
-import Stars from "../../components/Product/Stars";
 import Button from "../UI/Button";
 import cls from "./SidebarItems.module.scss";
 
