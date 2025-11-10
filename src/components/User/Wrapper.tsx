c58df903364f2791abf834b3e57a25438f0e62a5
corechuv <chuv.ole@icloud.com>
2025-11-10 21:18:25 +0100

    k

--- src/components/User/Wrapper.tsx
+++ src/components/User/Wrapper.tsx
@@ -13,12 +13,17 @@ const Wrapper: React.FC<UserProps> = ({ photoUrl, fullname, username }) => {
     return (
         <section className={s.profile}>
             <div className={s.profile__photo}>
-                <img
-                    className={s["profile__photo--img"]}
-                    src={photoUrl}
-                    alt=""
-                    loading="lazy"
-                />
+                {photoUrl ?
+                    <img
+                        className={s["profile__photo--img"]}
+                        src={photoUrl}
+                        alt=""
+                        loading="lazy"
+                    />
+                    :
+                    <div className={s["profile__photo--no"]}></div>
+                }
+
             </div>
             <div className={s.profile__identity}>
                 {fullname &&
