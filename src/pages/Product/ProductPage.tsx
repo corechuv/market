fbe01318b62446c6317d852e55eb9731b84cff31
corechuv <chuv.ole@icloud.com>
2025-11-15 02:17:39 +0100

    k

--- src/pages/Product/ProductPage.tsx
+++ src/pages/Product/ProductPage.tsx
@@ -276,15 +276,6 @@ export default function ProductPage() {
         <Breadcrumbs crumbs={categoryCrumbs as any} />
         */}
 
-        <div>
-
-          <h1 className={cls.productName}>{product.name}</h1>
-          <div className={cls.productMeta}>
-            <RatingBadge size="small" ratingValue={ratingValue} reviewCount={reviewCount} />
-            <div className={cls.productMeta__articleNumber}>Art.-Nr.: {articleNumber}</div>
-          </div>
-        </div>
-
         <div className={cls.productDetails}>
           <ProductImages images={images} />
 
@@ -301,6 +292,13 @@ export default function ProductPage() {
             <>
               <div className={cls.productInfo}>
                 <div className={cls.productTitle}>
+                  <div style={{ marginBottom: "10px" }}>
+                    <h1 className={cls.productName}>{product.name}</h1>
+                    <div className={cls.productMeta}>
+                      <RatingBadge size="small" ratingValue={ratingValue} reviewCount={reviewCount} />
+                      <div className={cls.productMeta__articleNumber}>Art.-Nr.: {articleNumber}</div>
+                    </div>
+                  </div>
                   <div className={cls.productPrice}>
                     <div className={cls.meta__container}>
                       <div className={"cls.meta_container--item"}>
