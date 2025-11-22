bee734e122d73d94a8a735f6df35b70b9bc4ee2f
corechuv <chuv.ole@icloud.com>
2025-11-22 15:02:09 +0100

    f

--- src/components/Navigation/Panel/SettingsPanel.tsx
+++ src/components/Navigation/Panel/SettingsPanel.tsx
@@ -15,6 +15,8 @@ import AddressIcon from "../../Icons/AddressIcon";
 import { applyTheme, getInitialTheme, type Theme } from "../../../utils/theme/theme";
 import { useLang } from "../../../context/LangContext";
 import type { AppLanguage } from "../../../utils/lang/lang";
+import ScrollArea from "../../UI/ScrollArea/ScrollArea";
+import { useVisualViewport } from "../../../hooks/useViewportUnits";
 
 interface SettingsPanelProps {
     /** Управление видимостью извне (Navigation) */
@@ -35,6 +37,7 @@ const SettingsPanel: React.FC<SettingsPanelProps> = ({
     onMouseEnter,
     onMouseLeave,
 }) => {
+    useVisualViewport();
     if (!open) return null;
 
     const nav = useNavigate()
@@ -75,114 +78,118 @@ const SettingsPanel: React.FC<SettingsPanelProps> = ({
             onMouseLeave={onMouseLeave}
             className={c.g}
         >
-            <MasterBar title="Settings" background="var(--n-bg-desktop)" />
-            <ul className={c.list}>
-                <li className={c.list__item} onClick={() => { nav("/account/notifications") }}>
-                    <NotificationIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Notifications
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/account/profile/edit") }}>
-                    <AccountIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Edit profile
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/account/orders") }}>
-                    <OrdersIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Orders
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/account/addresses") }}>
-                    <AddressIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Addresses
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/account/security") }}>
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Security
-                    </span>
-                </li>
-            </ul>
-            <h3 className={c.subtitle}>Theme</h3>
-            <ul className={c.list}>
-                <li className={c.list__item} aria-checked={theme === 'light'} onClick={() => { setTheme('light') }}>
-                    <SunIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={`Light`} title="light">
-                        Light
-                    </span>
-                </li>
-                <li className={c.list__item} aria-checked={theme === 'dark'} onClick={() => { setTheme('dark') }}>
-                    <MoonIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={`Dark`} title="dark">
-                        Dark
-                    </span>
-                </li>
-            </ul>
-            <h3 className={c.subtitle}>Language</h3>
-            <ul className={c.list}>
-                <li
-                    className={c.list__item}
-                    aria-checked={lang === "en"}
-                    onClick={() => handleChangeLanguage("en")}
-                >
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]}>English</span>
-                </li>
+            <div className={c.content}>
+                <MasterBar title="Settings" background="var(--n-bg-desktop)" />
+                <ScrollArea lockBody={false}>
+                    <ul className={c.list}>
+                        <li className={c.list__item} onClick={() => { nav("/account/notifications") }}>
+                            <NotificationIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Notifications
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/account/profile/edit") }}>
+                            <AccountIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Edit profile
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/account/orders") }}>
+                            <OrdersIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Orders
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/account/addresses") }}>
+                            <AddressIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Addresses
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/account/security") }}>
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Security
+                            </span>
+                        </li>
+                    </ul>
+                    <h3 className={c.subtitle}>Theme</h3>
+                    <ul className={c.list}>
+                        <li className={c.list__item} aria-checked={theme === 'light'} onClick={() => { setTheme('light') }}>
+                            <SunIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={`Light`} title="light">
+                                Light
+                            </span>
+                        </li>
+                        <li className={c.list__item} aria-checked={theme === 'dark'} onClick={() => { setTheme('dark') }}>
+                            <MoonIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={`Dark`} title="dark">
+                                Dark
+                            </span>
+                        </li>
+                    </ul>
+                    <h3 className={c.subtitle}>Language</h3>
+                    <ul className={c.list}>
+                        <li
+                            className={c.list__item}
+                            aria-checked={lang === "en"}
+                            onClick={() => handleChangeLanguage("en")}
+                        >
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]}>English</span>
+                        </li>
 
-                <li
-                    className={c.list__item}
-                    aria-checked={lang === "ru"}
-                    onClick={() => handleChangeLanguage("ru")}
-                >
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]}>Russian</span>
-                </li>
+                        <li
+                            className={c.list__item}
+                            aria-checked={lang === "ru"}
+                            onClick={() => handleChangeLanguage("ru")}
+                        >
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]}>Russian</span>
+                        </li>
 
-                <li
-                    className={c.list__item}
-                    aria-checked={lang === "de"}
-                    onClick={() => handleChangeLanguage("de")}
-                >
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]}>German</span>
-                </li>
-            </ul>
-            <h3 className={c.subtitle}>Information</h3>
-            <ul className={c.list}>
-                <li className={c.list__item} onClick={() => { nav("/about") }}>
-                    <AboutIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        About
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/help") }}>
-                    <HelpSupportIcon className={c["list__item--icon-left"]} />
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Help & Support
-                    </span>
-                </li>
-                <li className={c.list__item} onClick={() => { nav("/legal") }}>
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Legal
-                    </span>
-                </li>
-            </ul>
-            <h3 className={c.subtitle}></h3>
+                        <li
+                            className={c.list__item}
+                            aria-checked={lang === "de"}
+                            onClick={() => handleChangeLanguage("de")}
+                        >
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]}>German</span>
+                        </li>
+                    </ul>
+                    <h3 className={c.subtitle}>Information</h3>
+                    <ul className={c.list}>
+                        <li className={c.list__item} onClick={() => { nav("/about") }}>
+                            <AboutIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                About
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/help") }}>
+                            <HelpSupportIcon className={c["list__item--icon-left"]} />
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Help & Support
+                            </span>
+                        </li>
+                        <li className={c.list__item} onClick={() => { nav("/legal") }}>
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Legal
+                            </span>
+                        </li>
+                    </ul>
+                    <h3 className={c.subtitle}></h3>
 
-            <ul className={c.list}>
-                <li className={c.list__item} onClick={onLogout}>
-                    <svg className={c["list__item--icon-left"]}></svg>
-                    <span className={c["list__item--label"]} aria-label={``} title="">
-                        Logout
-                    </span>
-                </li>
-            </ul>
+                    <ul className={c.list}>
+                        <li className={c.list__item} onClick={onLogout}>
+                            <svg className={c["list__item--icon-left"]}></svg>
+                            <span className={c["list__item--label"]} aria-label={``} title="">
+                                Logout
+                            </span>
+                        </li>
+                    </ul>
+                </ScrollArea>
+            </div>
         </section>
     );
 };
